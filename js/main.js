12fe8afce8860032b1dacd4666164fe20d3eaa24
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-17 15:36:34 +0100

    .2 Inserimento album tramite handlebars

--- js/main.js
+++ js/main.js
@@ -6,5 +6,39 @@
 */
 
 $(document).ready(function() {
+    var source = $("#card-template").html();
+    var cardTemplate = Handlebars.compile(source);
 
+
+    $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        method: "GET",
+        success: function (data) {
+            // console.log(data);
+            // console.log(data.response);     //DO IL NOME ALL'ARRAY DATA.RESPONSE COSì DA POTERLO CICLARE
+            var arrayCanzoni = data.response;
+            for (var i = 0; i < arrayCanzoni.length; i++) {
+                // console.log(arrayCanzoni[i].poster);    //INSERISCO IL DATO IN UNA VARIABILE
+                var sorgenteAlbum = arrayCanzoni[i].poster;
+                // console.log(sorgenteAlbum);              //FACCIO LA STESSA COSA CON IL TITOLO, AUTORE E ANNO
+                var titolo =  arrayCanzoni[i].title;
+                var autore = arrayCanzoni[i].author;
+                var year = arrayCanzoni[i].year;
+                //ORA INSERISCO TUTTI I DATI NEL TEMPLATE DI HANDLEBARS
+                var template = {
+                    sourceAlbum: sorgenteAlbum,
+                    album: titolo,
+                    artista: autore,
+                    anno: year
+                }
+
+                var cardAlbum = cardTemplate(template);
+                $('.container-carte').append(cardAlbum);
+
+            }
+        },
+        error: function () {
+            alert('ERRORE GENERICO');
+        }
+    })
 });
